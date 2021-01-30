0572e60cf2faa96dc7a7be2f1b2bc86b0645c059
DanielFaw <58899301+DanielFaw@users.noreply.github.com>
2021-01-30 16:54:24 -0500

    basis of the mineable item class i think

--- Scripts/InventoryAndCrafting/MineableItem.cs
+++ Scripts/InventoryAndCrafting/MineableItem.cs
@@ -3,15 +3,24 @@ using System;
 
 public class MineableItem : Node
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+    [Export] int amountOfItemsToSpawn = 5;
+    [Export] int hardness = 1;
+    [Export] int itemIdDropped = 0;
 
-    // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
     }
+
+    public void HitItem()
+    {
+        amountOfItemsToSpawn--;
+        if(amountOfItemsToSpawn == 0)
+        {
+            //drop
+        }
+    }
+
+    
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
 //  public override void _Process(float delta)
