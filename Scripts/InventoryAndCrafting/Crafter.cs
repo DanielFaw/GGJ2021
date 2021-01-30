8d8b36afaf6ca3856a201b4743ef437da8c19059
DanielFaw <58899301+DanielFaw@users.noreply.github.com>
2021-01-29 19:15:24 -0500

    Crafting and inventory fully implemented
    Now we just need to get UI elements hooked up and it should be good to go

--- Scripts/InventoryAndCrafting/Crafter.cs
+++ Scripts/InventoryAndCrafting/Crafter.cs
@@ -8,32 +8,18 @@ public class Crafter : Node
 	//Never address these on their own; only use helper functions to input/output info from them!
 	//These have to be dictionaries unless we want to add a null crafting recipe for everything that can't be directly crafted, which would be kinda dumb
 	Dictionary<int, List<int>> craftingDict;
+	Dictionary<int, int> madePerCraft;
 	Dictionary<int, List<int>> recipeDict;
+
 	
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		craftingDict = new Dictionary<int, List<int>>();
+		madePerCraft = new Dictionary<int, int>();
 		recipeDict = new Dictionary<int, List<int>>();
-		/*
-		AddRecipe(1, new List<int>{2,2,5,3});
-		AddRecipe(2, new List<int>{2,3,2,4});
-		AddRecipe(3, new List<int>{2,2});
-		AddRecipe(4, new List<int>{7,6});
-		AddRecipe(5, new List<int>{10,7});
-		AddRecipe(6, new List<int>{10, 69});
-		AddRecipe(8, new List<int>{1, 2, 3, 4, 5, 6});
-		AddRecipe(9, new List<int>{5, 8});
-		AddRecipe(69, new List<int>{420,420});
-		
-		//GD.Print("Player's fake inventory contains 2,2,3,3,4,4,9,7,10,6,10,69");
-		List<int> sneakyInventory = new List<int>{2,2,3,3,4,4,9,7,100,6,10,69,1,20, 5, 8, 420, 420};
-		//Player can craft: ID2, ID3, ID4, ID6, ID8
-		
-		CheckCrafting(sneakyInventory);
-		*/
-		
+		InitiateRecipes();
 	}
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
 //  public override void _Process(float delta)
@@ -41,13 +27,18 @@ public class Crafter : Node
 //      
 //  }
 
+	private void InitiateRecipes()
+	{
+		//Make all of the crafting recipes here
+	}
+
 
 
 
 	//The formatted array must be as follows:
 		//[amountOfItem, itemID, amountOfItemN, ItemNId]
 		//This can go on however long you like, but it must be amount,id even if it is just 1 item/amount
-	private void AddRecipe(int IdOfItem, List<int> formattedArray)
+	private void AddRecipe(int IdOfItem, List<int> formattedArray, int amountCreated = 1)
 	{
 		if(formattedArray.Count % 2 != 0)
 		{
@@ -55,6 +46,7 @@ public class Crafter : Node
 		}
 		
 		craftingDict.Add(IdOfItem, formattedArray);
+		madePerCraft.Add(IdOfItem, amountCreated);
 		
 		//Adds the crafted item as a craftable for all the ingredients, for faster searching later
 		for(int i = 1; i <= formattedArray.Count; i+=2)
@@ -73,6 +65,31 @@ public class Crafter : Node
 			
 		}
 	}
+
+	public List<int> GetRecipe(int itemId)
+	{
+		if(craftingDict.ContainsKey(itemId))
+		{
+			return craftingDict[itemId];
+		}
+		else
+		{
+			return null;
+		}
+	}
+
+	public int AmountCreated(int itemId)
+	{
+		if(madePerCraft.ContainsKey(itemId))
+		{
+			return madePerCraft[itemId];
+		}
+		else
+		{
+			return -1;
+		}
+		
+	}
 	
 	
 	private bool IsMaterial(int IdOfItem)
@@ -81,15 +98,19 @@ public class Crafter : Node
 		
 	}
 	
-	public void CheckCrafting(List<int> playerInventory)
+	public List<int> CheckCrafting(List<int> playerInventory)
 	{
+		List<int> craftableItems = new List<int>();
 		foreach(KeyValuePair<int, List<int>> item in craftingDict)
 		{
 			if(CheckCrafting(item.Key, playerInventory))
 			{
 				//This item can be crafted!
+				craftableItems.Add(item.Key);			
+
 			}
 		}
+		return craftableItems;
 	}
 	
 	
--- Scripts/InventoryAndCrafting/PlayerInventory.cs
+++ Scripts/InventoryAndCrafting/PlayerInventory.cs
@@ -1,17 +1,18 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public class PlayerInventory : Node
 {
-	
-	// Declare member variables here. Examples:
-	// private int a = 2;
-	// private string b = "text";
+	Dictionary<int, int> inventory;
+	Crafter craftRef;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		
+		inventory = new Dictionary<int, int>();
+		craftRef = GetNode("../CrafterScript") as Crafter;
+		SetInventory();
 	}
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -19,4 +20,79 @@ public class PlayerInventory : Node
 //  {
 //      
 //  }
+
+	private void SetInventory()
+	{
+		//Add whatever the player starts out with here
+	}
+
+	private void PrintInventory()
+	{
+		foreach(KeyValuePair<int, int> item in inventory)
+		{
+			GD.Print("Amount of item " + item.Key + ": " + item.Value);
+		}
+	}
+
+	public void AddItem(int itemAmount, int itemId)
+	{
+		if(inventory.ContainsKey(itemId))
+		{
+			inventory[itemId] += itemAmount;
+		}
+		else
+		{
+			inventory.Add(itemId, itemAmount);
+		}
+	}
+	
+	public void RemoveItem(int itemId, int itemAmount)
+	{
+		if(inventory.ContainsKey(itemId))
+		{
+			inventory[itemId] -= itemAmount;
+			if (inventory[itemId] < 1)
+			{
+				inventory.Remove(itemId);
+			}
+		}
+	}
+	
+	/// <summary>
+	/// Craft the specified item. Returns true if it was successful.
+	/// </summary>
+	/// <param name="itemId"></param>
+	public bool CraftItem(int itemId)
+	{
+		if(inventory.Count < 1)
+		{
+			return false;
+		}
+		//Convert the dictionary into a list
+		List<int> convertedInventory = new List<int>();
+		foreach(KeyValuePair<int, int> item in inventory)
+		{
+			convertedInventory.Add(item.Value);
+			convertedInventory.Add(item.Key);
+		}
+
+		//call the crafter script
+
+		if(craftRef.CheckCrafting(itemId, convertedInventory))
+		{
+			//Craft dis item!
+			List<int> recipe = craftRef.GetRecipe(itemId);
+			for(int i = 1; i <= recipe.Count; i+= 2)
+			{
+				RemoveItem(recipe[i], recipe[i - 1]);
+			}
+			AddItem(itemId, craftRef.AmountCreated(itemId));
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+		
+	}
 }
