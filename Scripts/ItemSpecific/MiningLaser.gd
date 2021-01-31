5c861ac53e0686ec6d3e5b5067e9e80a0e9e4235
Ethan Ruhl <epruhl102@gmail.com>
2021-01-30 20:54:16 -0500

    Help

--- Scripts/ItemSpecific/MiningLaser.gd
+++ Scripts/ItemSpecific/MiningLaser.gd
@@ -2,33 +2,45 @@ extends Spatial
 
 const RESOURCE = preload("res://Scripts/InventoryAndCrafting/MineableItem.cs");
 
+const BEAMOBJECT = preload("res://Objects/Misc/LaserBeamEffect.tscn");
+
 export var miningPower:float = 1;
+
 
 var previousResource;
 var currentResource;
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 
 >>>>>>> parent of abb1239... Added laser effects, basic resource logic
+=======
+var beamEffectObject;
+>>>>>>> parent of 823fa93... Fixed laserScript
 
-var beam;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 <<<<<<< HEAD
+<<<<<<< HEAD
 	beam = $Beam;
 =======
 >>>>>>> parent of abb1239... Added laser effects, basic resource logic
+=======
+	beamEffectObject = $LaserTip/Beam
+>>>>>>> parent of 823fa93... Fixed laserScript
 	pass # Replace with function body.
 
 
 func _process(delta):
 <<<<<<< HEAD
 
+	#Beam effect
 	if(currentResource != null && currentResource.isMining):
-		beam.visible = true;
+		beamEffectObject.visible = true;
 	else:
-		beam.visible = false;
+		beamEffectObject.visible = false;
+
 
 =======
 >>>>>>> parent of abb1239... Added laser effects, basic resource logic
@@ -36,13 +48,14 @@ func _process(delta):
 		if(currentResource != null):
 			#Start mining the resource
 			currentResource.MineResource(miningPower);
+		
+		
 		else:
 			#Prevent player from mining resource after walking away
 			if(previousResource != null):
 				if(previousResource.isMining):
 					previousResource.StopMining();
 					pass;
-
 	elif(!Input.is_action_pressed("player_fire_right") && currentResource != null):
 		currentResource.StopMining();
 
@@ -54,8 +67,6 @@ func BodyEnter(var body):
 	var parent = body.get_parent();
 	if((parent as RESOURCE) != null && currentResource == null):
 		currentResource = (parent as RESOURCE);
-		
-		#Connect "TakeDamage" so the 
 		currentResource.connect("ResourceMined",self,"TakeDamage");
 		if(previousResource == null):
 			previousResource = currentResource;
