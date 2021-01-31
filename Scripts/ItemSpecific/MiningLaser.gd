541d2140e7bc59493db9102a0fe8ff85ae161780
Ethan Ruhl <epruhl102@gmail.com>
2021-01-30 21:16:08 -0500

    I think it's fixed

--- Scripts/ItemSpecific/MiningLaser.gd
+++ Scripts/ItemSpecific/MiningLaser.gd
@@ -6,39 +6,17 @@ export var miningPower:float = 1;
 
 var previousResource;
 var currentResource;
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 
->>>>>>> parent of abb1239... Added laser effects, basic resource logic
-=======
 var beamEffectObject;
->>>>>>> parent of 823fa93... Fixed laserScript
-=======
-
->>>>>>> parent of abb1239... Added laser effects, basic resource logic
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-	beam = $Beam;
-=======
->>>>>>> parent of abb1239... Added laser effects, basic resource logic
-=======
-	beamEffectObject = $LaserTip/Beam
->>>>>>> parent of 823fa93... Fixed laserScript
-=======
->>>>>>> parent of abb1239... Added laser effects, basic resource logic
+
 	pass # Replace with function body.
 
 
 func _process(delta):
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 
 	#Beam effect
 	if(currentResource != null && currentResource.isMining):
@@ -46,11 +24,6 @@ func _process(delta):
 	else:
 		beamEffectObject.visible = false;
 
-
-=======
->>>>>>> parent of abb1239... Added laser effects, basic resource logic
-=======
->>>>>>> parent of abb1239... Added laser effects, basic resource logic
 	if(Input.is_action_just_pressed("player_fire_right")):
 		if(currentResource != null):
 			#Start mining the resource
